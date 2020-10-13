3aa4d8155c2f75c10d546dce217f3bd7533e59d0
Mutatina Abel <amutatina@caa.gov.rw>
2020-10-13 17:19:08 +0200

    static file

--- stockapp/settings.py
+++ stockapp/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'stockapp/static')
 ]
 AUTH_USER_MODEL = 'users.User'
 # Controlling uploaded media
