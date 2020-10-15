4033e1077249185218a50dd88307d9c84b255b34
Mutatina Abel <amutatina@caa.gov.rw>
2020-10-16 00:40:59 +0200

    trying to test the last

--- users/api/views.py
+++ users/api/views.py
@@ -7,7 +7,6 @@ from users.api.serializers import UserRegSerializer
 
 
 @api_view(['POST', ])
-# @permission_classes((IsAuthenticated,))
 def registration_view(request):
     if request.method == 'POST':
         serializer = UserRegSerializer(data=request.data)
