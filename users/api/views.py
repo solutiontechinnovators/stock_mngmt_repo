72b711e0aa0aeff2386a484f8fa9a3c3b58f5093
Mutatina Abel <amutatina@caa.gov.rw>
2020-10-16 11:26:34 +0200

    some changes in static and api views

--- users/api/views.py
+++ users/api/views.py
@@ -1,12 +1,15 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 
 from users.api.serializers import UserRegSerializer
 
 
 @api_view(['POST', ])
+# @permission_classes((IsAuthenticated,))
+@authentication_classes([])
+@permission_classes([])
 def registration_view(request):
     if request.method == 'POST':
         serializer = UserRegSerializer(data=request.data)
