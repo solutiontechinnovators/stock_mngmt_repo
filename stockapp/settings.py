66405aec222f73f700bc6f3886d6dbe1fb407b99
DanK <dkarake@caa.gov.rw>
2020-10-27 12:09:19 +0200

    pycache

--- stockapp/settings.py
+++ stockapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'h=#d2%tq9-i6wu!r+ihqtc4+5=4+wee+1#8qeu5iq-cs*@r=ry'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['stockapprealproject.herokuapp.com', ]
+ALLOWED_HOSTS = []
 # testing
 
 # Application definition
@@ -94,13 +94,15 @@ WSGI_APPLICATION = 'stockapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd64ppfcs77t4m9',
-        'USER': 'wryjxjdylqqnry',
-        'PASSWORD': '36f769ec1e2052ec8c1cbae839ff0fcc7bcbbd4e93e7da9bfb41495b3b71e856',
-        'HOST': 'ec2-52-3-4-232.compute-1.amazonaws.com',
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stockapp',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
@@ -144,7 +146,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'stockapp/static')
+    os.path.join(BASE_DIR, '/static')
 ]
 AUTH_USER_MODEL = 'users.User'
 # Controlling uploaded media
