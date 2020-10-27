c66830bb6753d8acda87660070f5b9aa59fc8939
Mutatina Abel <amutatina@caa.gov.rw>
2020-10-27 11:52:16 +0200

    modes of administration added.

---
+++ adminstration/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+
+# ***************Position Model***************
+
+
+class Position(models.Model):
+    position_name = models.CharField(max_length=100)
+    position_code = models.CharField(max_length=20)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.position_name
+
+    class Meta:
+        db_table = "position"
+
+# *******************USER_POSITION_ASSIGNMENT MODEL******************
+
+
+class UserPositionAssignment(models.Model):
+    user = models.ForeignKey(
+        User, related_name='action_by', on_delete=models.PROTECT)
+    position = models.ForeignKey(Position, on_delete=models.PROTECT)
+    assignment_status = models.CharField(max_length=20)
+    supervisor = models.ForeignKey(
+        User, related_name='supervisor', on_delete=models.PROTECT)
+    assigned_by = models.ForeignKey(
+        User, related_name='assigned_by', on_delete=models.PROTECT)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.assignment_status
+
+    class Meta:
+        db_table = "user_position_asignment"
+
+
+class Shop(models.Model):
+    shop_name = models.CharField(max_length=100)
+    shop_no = models.IntegerField()
+    sector = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.shop_name
+
+    class Meta:
+        db_table = "shop"
--- users/api/views.py
+++ users/api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from users.api.serializers import UserRegSerializer
+from django.core import serializers
+import json
 
 # testing login to return token and user details
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -34,6 +36,10 @@ class CustomObtainAuthToken(ObtainAuthToken):
         response = super(CustomObtainAuthToken, self).post(
             request, *args, **kwargs)
         token = Token.objects.get(key=response.data['token'])
-        user = User.objects.get(id=token.user_id)
+        user = serializers.serialize(
+            "json", User.objects.all(), fields=('first_name'))
+        # user = User.objects.filter(id=token.user_id)
         print(user)
-        return Response({'token': token.key, 'user_email': user.email, 'first_name': user.first_name, 'last_name': user.last_name})
+
+        # return Response({'token': token.key, 'user_email': user.email, 'first_name': user.first_name, 'last_name': user.last_name})
+        return Response({'token': token.key, 'user': user})
